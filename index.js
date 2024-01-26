a7e986984f73edbda781ab3ceb85a8b3be1f3cd4
shaankal <shaankal@hotmail.com>
2024-01-25 21:33:56 -0800

    initialized base model

---
+++ index.js
@@ -0,0 +1,23 @@
+import { Configuration, OpenAIApi } from 'openai';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+});
+
+const openai = new OpenAIApi(configuration);
+
+async function main() {
+
+    const chatCompletion = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+            { role: 'user' , content: 'What is the NFL (National Football League) ?'}
+        ]
+    });
+
+    console.log(chatCompletion)
+}
+
+main();
