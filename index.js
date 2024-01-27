f592c36e1f9d32ee2b262fafaeca39aea8cf8998
shaankal <shaankal@hotmail.com>
2024-01-26 19:19:02 -0800

    terminal chatbot init

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
 import OpenAI from "openai";
 import dotenv from "dotenv";
+import readlineSync from 'readline-sync';
+import colors from 'colors';
+
 dotenv.config();
 
 const openai = new OpenAI({
@@ -8,15 +11,34 @@ const openai = new OpenAI({
 });
 
 async function main() {
+    console.log(colors.bold.blue('Welcome to the greatest Chatbot in history!'))
+    console.log(colors.bold.blue('You now have the honor to chat with me!'))
 
-    const chatCompletion = await openai.chat.completions.create({
-        model: 'gpt-3.5-turbo',
-        messages: [
-            { role: 'user' , content: 'What is the NFL (National Football League) ?'}
-        ]
-    });
+    while(true) {
+        const userInput = readlineSync.question(colors.bold.magenta('You: '));
 
-    console.log(chatCompletion)
+        try {
+            
+            const chatCompletion = await openai.chat.completions.create({
+                model: 'gpt-3.5-turbo',
+                messages: [
+                    { role: 'user' , content: userInput}
+                ]
+            })
+
+            const completionText = chatCompletion.data.choices[0].message.content;
+
+            if (userInput.toLowerCase() == 'exit') {
+                console.log(colors.green('Bot: ') + completionText);
+                return;
+            }
+
+            console.log(colors.green('Bot: ') + completionText);
+        } catch (error) {
+            console.error(colors.red(error));
+        }
+    }
+
 }
 
 main();
