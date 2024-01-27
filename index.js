6116e75a16f70dbfd67c4b0eb0dad8fc9b0faa36
shaankal <shaankal@hotmail.com>
2024-01-26 19:27:58 -0800

    allow for history storage with chat

--- index.js
+++ index.js
@@ -14,16 +14,22 @@ async function main() {
     console.log(colors.bold.blue('Welcome to the greatest Chatbot in history!'))
     console.log(colors.bold.blue('You now have the honor to chat with me!'))
 
+    const chatHistorySaver = []; //Store conversation with Chatbot
+
     while(true) {
         const userInput = readlineSync.question(colors.bold.magenta('You: '));
 
         try {
-            
+            //Storing history, through iteration
+            const messages = chatHistorySaver.map(([role, content]) => ({ role, content}));
+
+            //Update input
+            messages.push({role: 'user' , content: userInput})
+
+            //Gets user input
             const chatCompletion = await openai.chat.completions.create({
                 model: 'gpt-3.5-turbo',
-                messages: [
-                    { role: 'user' , content: userInput}
-                ]
+                messages: messages, 
             })
 
             const completionText = chatCompletion.data.choices[0].message.content;
@@ -34,6 +40,9 @@ async function main() {
             }
 
             console.log(colors.green('Bot: ') + completionText);
+
+            //Update History with input 
+            //Need to do
         } catch (error) {
             console.error(colors.red(error));
         }
